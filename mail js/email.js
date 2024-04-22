31a4763ce8947e9b23cca3a56e17bfba266d9990
damirusso95 <damirusso95@gmail.com>
2024-04-22 18:34:12 +0200

    email valide

---
+++ mail js/email.js
@@ -0,0 +1,27 @@
+// 1)Chiedi all’utente la sua email
+// 2) controllare che sia nella lista di chi può accedere.
+// 3)Stampa un messaggio SOLO SE l'utente è in lista.
+
+
+// chiedo email all'utente
+let emailUtente = prompt("inserisci la tua Email")
+// console.log(emailUtente);
+
+// creo una lista di email valide utilizzando un array
+const emailValide = [
+    "damiano@gmail.com",
+    "luca@gmail.com",
+    "gino@gmail.com",
+    "marco@gmail.com",
+    "java@gmail.com",
+    "peppe@gmail.com"
+];
+// console.log(emailValide);
+
+
+// se l'email è nella lista allora stampo
+// // if(emailUtente = emailValide){
+//     console.log("Email valida")
+
+// }
+
